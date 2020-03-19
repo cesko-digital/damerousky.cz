f4b655c542ec7b9bb54deeb89dfca8bb7e13cd97
Filip Jirsák <filip@jirsak.org>
2020-03-19 19:40:55 +0100

    Základní šablona Gridsome.

---
+++ src/pages/Index.vue
@@ -0,0 +1,32 @@
+<template>
+    <Layout>
+        <h1>DámeRoušky.cz</h1>
+        <h2>Šijete roušky? Přidejte se na mapu!</h2>
+        <p>Přidejte nové místo do kategorie “nabízím/šiju roušky”. Do popisu přidejte kontakt na vás (ideálně e-mail). A nastavte, zda jsou “zdarma” anebo
+            “placené“.</p>
+        <p>Pokud nestíháte šít, nebo vám došel materiál, napište tuto informaci do vašeho kontaktu na mapě.</p>
+
+        <h2>Sháníte roušky?</h2>
+
+        <p>Najděte si kontakt ve svém okolí a napište mu na e-mail uvedený v popisu místa (nalevo). Buďte prosím tolerantní a slušní, poptávka významně
+            převyšuje nabídku.</p>
+
+        <h2>Máte problém?</h2>
+
+        <p>Napište nám <a href="mailto:damerousky@gmail.com">damerousky@gmail.com</a>.</p>
+    </Layout>
+</template>
+
+<script>
+    export default {
+        metaInfo: {
+            title: 'Hello, world!'
+        }
+    }
+</script>
+
+<style>
+    .home-links a {
+        margin-right: 1rem;
+    }
+</style>
