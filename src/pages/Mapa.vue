61451848e24b5fda38bf9788cf80f9967f296778
Filip Jirsák <filip@jirsak.org>
2020-03-20 12:38:11 +0100

    Mapu vkládat přes doménu Mapotic.

--- src/pages/Mapa.vue
+++ src/pages/Mapa.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-    const baseMapURL = 'https://www.damerousky.cz/';
+    const baseMapURL = 'https://www.mapotic.com/rouskyven/';
 
     export default {
         data: () => ({
