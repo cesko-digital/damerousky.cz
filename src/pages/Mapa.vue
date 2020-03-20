a11a07992a29c96d14a53e07ae73627a71024305
Filip Jirsák <filip@jirsak.org>
2020-03-20 11:32:46 +0100

    Přesměrování ID místa do vložené mapy.
    https://www.damerousky.cz/000000-xxxxxxxxx přesměruje na https://www.damerousky.cz/mapa/?id=000000-xxxxxxxxx

--- src/pages/Mapa.vue
+++ src/pages/Mapa.vue
@@ -1,5 +1,20 @@
 <template>
     <Layout>
-        <iframe src="https://www.damerousky.cz" style="width: 100%; height: 100%;"/>
+        <iframe :src="mapURL" style="width: 100%; height: 100%;"/>
     </Layout>
 </template>
+
+<script>
+    export default {
+        data: () => ({
+            mapURL: 'https://www.damerousky.cz/'
+        }),
+        created() {
+            let params = (new URL(document.location)).searchParams
+            let id = params.get('id');
+            if (id != null) {
+                this.mapURL = 'https://www.damerousky.cz/' + id;
+            }
+        }
+    }
+</script>
