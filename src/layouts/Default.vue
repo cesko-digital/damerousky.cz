809f5cc91c3858e728fb8d12fe1d7116f59cb854
Filip Jirsák <filip@jirsak.org>
2020-03-21 20:12:54 +0100

    Nové položky menu Informace.

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -12,14 +12,34 @@
                         <v-btn text append="" v-on="on">Informace</v-btn>
                     </template>
                     <v-list>
+                        <v-list-item link href="/vytvorit-misto">
+                            <v-list-item-content>
+                                <v-list-item-title>Jak vytvořit nové místo</v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
                         <v-list-item link href="/najit-mista">
                             <v-list-item-content>
                                 <v-list-item-title>Jak najít nejbližší místa s rouškou</v-list-item-title>
                             </v-list-item-content>
                         </v-list-item>
-                        <v-list-item link href="/vytvorit-misto">
+                        <v-list-item link href="/ziskat-rousku">
                             <v-list-item-content>
-                                <v-list-item-title>Jak vytvořit nové místo</v-list-item-title>
+                                <v-list-item-title>Jak získám roušku?</v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                        <v-list-item link href="/vyroba-a-pece">
+                            <v-list-item-content>
+                                <v-list-item-title>Jak vyrobit roušku a jak se o ni starat?</v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                        <v-list-item link href="/nesiju">
+                            <v-list-item-content>
+                                <v-list-item-title>Jak pomoci, když nešiju?</v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                        <v-list-item link href="/opravy-sicich-stroju">
+                            <v-list-item-content>
+                                <v-list-item-title>Opravy šicích strojů</v-list-item-title>
                             </v-list-item-content>
                         </v-list-item>
                         <v-list-item link href="/pro-seniory">
@@ -32,11 +52,23 @@
                                 <v-list-item-title>Časté dotazy (a odpovědi)</v-list-item-title>
                             </v-list-item-content>
                         </v-list-item>
+                        <v-list-item link href="/english-info">
+                            <v-list-item-content>
+                                <v-list-item-title>Basic information in English</v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                        <v-list-item link href="/web-a-data">
+                            <v-list-item-content>
+                                <v-list-item-title>O webu a vašich datech</v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+<!--
                         <v-list-item link href="/o-nas">
                             <v-list-item-content>
                                 <v-list-item-title>O nás</v-list-item-title>
                             </v-list-item-content>
                         </v-list-item>
+-->
                     </v-list>
                 </v-menu>
             </v-toolbar-items>
